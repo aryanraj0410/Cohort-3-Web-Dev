c64791fe6a031b87fc5a8defc655806419cde983
aryanraj0410 <ranjanayush287@gmail.com>
2025-07-09 18:23:29 +0530

    Till AsyncJS

--- Sync and Async/Callbacks.js
+++ Sync and Async/Callbacks.js
@@ -14,4 +14,5 @@ for(let i=0;i<=100000000;i++){
     a = a+1;
 }
 
-console.log("Entensive operation is done.")
+console.log("Entensive operation is done.") // this will be called firstt before the timeout function as the cpu is busy duing the 
+// extensive operation and will call the timeout after the thread is free
